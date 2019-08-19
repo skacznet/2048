a56d53049ebfb7b9eb443f36153876d185e789d9
skacznet <33492325+skacznet@users.noreply.github.com>
2019-08-19 10:41:43 +0200

    Bug fixing
    Fixed: new elements were being attached to the board even if making a move wasn't possible.

--- src/shared/gameLogic.js
+++ src/shared/gameLogic.js
@@ -17,6 +17,7 @@ const addRandomItem = (itemsArray) => {
 
 export const onLeft = (items) => {
     let updatedItems = [...items];
+    let itemsChanged = false;
     let indexesToMoveLeft = [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15];
     // Check all the elements which could be moved left. Start from the left.
     indexesToMoveLeft.forEach((i) => {
@@ -47,6 +48,7 @@ export const onLeft = (items) => {
                     // There are no items with the same value. Move an item as far as it is possible.
                     updatedItems[i-emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                     updatedItems[i].itemValue = 0;
+                    itemsChanged = true;
                 } else {
                     // Check if there is a clear path between items to connect.
                     // Take the closest one. If it is just on the left or the item on the right of it is empty, the path is clear.
@@ -63,23 +65,28 @@ export const onLeft = (items) => {
                         // Connect items
                         updatedItems[i-indexesToConnect[indexesToConnect.length - 1]].itemValue *= 2;
                         updatedItems[i].itemValue = 0;
+                        itemsChanged = true;
                     } else {
                         // There is something between items with the same value. Move the item as far as it is possible.
                         if(emptyIndexes.length > 0) { 
                             updatedItems[i-emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                             updatedItems[i].itemValue = 0;
+                            itemsChanged = true;
                         }
                     }
                 }
             }
         }
     });
-    updatedItems = addRandomItem(updatedItems);
+    if(itemsChanged) {
+        updatedItems = addRandomItem(updatedItems);
+    }
     return updatedItems;
 }
 
 export const onRight = (items) => {
     let updatedItems = [...items];
+    let itemsChanged = false;
     let indexesToMoveRight = [14, 13, 12, 10, 9, 8, 6, 5, 4, 2, 1, 0];
     // Check all the elements which could be moved right. Start from the right.
     indexesToMoveRight.forEach((i) => {
@@ -118,6 +125,7 @@ export const onRight = (items) => {
                     // There are no items with the same value. Move an item as far as it is possible.
                     updatedItems[i+emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                     updatedItems[i].itemValue = 0;
+                    itemsChanged = true;
                 } else {
                     // Check if there is a clear path between items to connect.
                     // Take the closest one. If it is just on the right or the item on the left of it is empty, the path is clear.
@@ -134,23 +142,28 @@ export const onRight = (items) => {
                         // Connectitems
                         updatedItems[i+indexesToConnect[indexesToConnect.length - 1]].itemValue *= 2;
                         updatedItems[i].itemValue = 0;
+                        itemsChanged = true;
                     } else {
                         // There is something between items with the same value. Move the item as far as it is possible.
                         if(emptyIndexes.length > 0) { 
                             updatedItems[i+emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                             updatedItems[i].itemValue = 0;
+                            itemsChanged = true;
                         }
                     }
                 }
             }
         }
     });
-    updatedItems = addRandomItem(updatedItems);
+    if(itemsChanged) {
+        updatedItems = addRandomItem(updatedItems);
+    }
     return updatedItems;
 }
 
 export const onUp = (items) => {
     let updatedItems = [...items];
+    let itemsChanged = false;
     // Check all the elements which could be moved up. Start from the top and move to the end.
     for(let i=4; i<16; i++) {
         // Only for elements with a value
@@ -180,6 +193,7 @@ export const onUp = (items) => {
                     // There are no items with the same value. Move an item as far as it is possible.
                     updatedItems[i-emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                     updatedItems[i].itemValue = 0;
+                    itemsChanged = true;
                 } else {
                     // Check if there is a clear path between items to connect.
                     // Take the closest one. If it is just above or the item below is empty, the path is clear.
@@ -196,23 +210,28 @@ export const onUp = (items) => {
                         // Connect items
                         updatedItems[i-indexesToConnect[indexesToConnect.length - 1]].itemValue *= 2;
                         updatedItems[i].itemValue = 0;
+                        itemsChanged = true;
                     } else {
                         // There is something between items with the same value. Move the item as far as it is possible.
                         if(emptyIndexes.length > 0) { 
                             updatedItems[i-emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                             updatedItems[i].itemValue = 0;
+                            itemsChanged = true;
                         }
                     }
                 }
             }
         }
     }
-    updatedItems = addRandomItem(updatedItems);
+    if(itemsChanged) {
+        updatedItems = addRandomItem(updatedItems);
+        }
     return updatedItems;
 }
 
 export const onDown = (items) => {
     let updatedItems = [...items];
+    let itemsChanged = false;
     // Check all the elements which could be moved down. Start from the bottom and move to the beginning.
     for(let i=11; i>=0; i--) {
         // Only for elements with a value
@@ -236,12 +255,13 @@ export const onDown = (items) => {
                 }
                 checkIndex = checkIndex - 4;
             }
-            // Action is required only if there are empty space below or there are elements with the same value
+            // Action is required only if there is empty space below or there are elements with the same value
             if(indexesToConnect.length > 0 || emptyIndexes.length > 0) {
                 if(indexesToConnect.length === 0) {
                     // There are no items with the same value. Move an item as far as it is possible.
                     updatedItems[i+emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                     updatedItems[i].itemValue = 0;
+                    itemsChanged = true;
                 } else {
                     // Check if there is a clear path between items to connect.
                     // Take the closest one. If it is just below or the item above it is empty, the path is clear.
@@ -258,17 +278,21 @@ export const onDown = (items) => {
                         // Connect items
                         updatedItems[i+indexesToConnect[indexesToConnect.length - 1]].itemValue *= 2;
                         updatedItems[i].itemValue = 0;
+                        itemsChanged = true;
                     } else {
                         // There is something between items with the same value. Move the item as far as it is possible. 
                         if(emptyIndexes.length > 0) {
                             updatedItems[i+emptyIndexes[0]].itemValue = updatedItems[i].itemValue;
                             updatedItems[i].itemValue = 0;
+                            itemsChanged = true;
                         }
                     }
                 }
             }
         }
     }
-    updatedItems = addRandomItem(updatedItems);
+    if(itemsChanged) {
+        updatedItems = addRandomItem(updatedItems);
+    }
     return updatedItems;
 }
